10ec14f8f9a9ad581c247351205fc865c5c5187b
Steven Murawski <stmuraws@microsoft.com>
2021-03-02 16:04:49 -0600

    adding the deployment pipeline

--- Infrastructure/Publish-MercuryHealthInfrastructure.ps1
+++ Infrastructure/Publish-MercuryHealthInfrastructure.ps1
@@ -1,91 +1,115 @@
 param (
     $ResourceGroupName = 'onpremtothecloud',
+    $ManagementResourceGroupName = ($ResourceGroupName + '-mgmt'),
     $StorageAccountName = 'optcdsc',
     $StorageContainerName = 'configurations',
-    $ConfigurationPath = 'MercHealthConfig.ps1',
-    [switch]$FreshStart
+    $Location = 'eastus',
+    $ApplicationArchivePath = '../MercuryHealth.zip',
+    [switch]$FreshStart,
+    [switch]$ManagementRGOnly,
+    [switch]$ApplicationRGOnly
 )
+# This is set in a variable in the ApplicationRG deployment.
+# If you change this, then you have to update the ARM template
+
+$ConfigurationPath = 'MercHealthConfig.ps1'
 
 if ($FreshStart) {
-    Remove-AzResourceGroup -Name $ResourceGroupName -Force
-    Remove-Item ./current.parameters.json -Force
+    if (-not $ManagementRGOnly) {
+        Write-Host ""; Write-Host "Removing $ResourceGroupName"
+        Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue |
+        Remove-AzResourceGroup -Force | 
+        Out-Null
+    }
+    if (-not $ApplicationRGOnly) {
+        Write-Host ""; Write-Host "Removing $ManagementResourceGroupName"
+        Get-AzResourceGroup -Name $ManagementResourceGroupName -ErrorAction SilentlyContinue |
+        Remove-AzResourceGroup -Force | 
+        Out-Null
+    }
+    Write-Host ""; Write-Host "Removing parameters file."
+    Remove-Item './current.parameters.json' -Force | Out-Null
+}
+
+$TemplateSpecParams = @{
+    Version           = '1.0.1'
+    ResourceGroupName = $ManagementResourceGroupName
+    Location = $Location
 }
 
 ### Policy?
 
-# Set up resource group
-New-AzResourceGroup -Name $ResourceGroupName -Location 'eastus'
-$TemplateSpecParams = @{
-    Version =  '1.0.2'
-    ResourceGroupName = $ResourceGroupName 
-    Location = 'eastus' 
+if (-not $ApplicationRGOnly) {
+    Write-Host ""; Write-Host "Setting up the management resource group - $ManagementResourceGroupName"
+    New-AzResourceGroup -Name $ManagementResourceGroupName -Location $Location -Force | Out-Null
+
+    Write-Host ""; Write-Host "Deploying template specs to the management resource group."
+    $ManagementRGSpec = New-AzTemplateSpec @TemplateSpecParams -Name ManagementRG -TemplateFile './ManagementRG.json'
+    $ManagementRGSpecId = $ManagementRGSpec.Versions |
+        Sort-Object -Property Name -Descending |
+        Select-Object -First 1 -ExpandProperty Id
+
+    $ApplicationRGSpec = New-AzTemplateSpec @TemplateSpecParams -Name ApplicationRG -TemplateFile './ApplicationRG.json'
+    $ApplicationRGSpecId = $ApplicationRGSpec.Versions |
+        Sort-Object -Property Name -Descending |
+        Select-Object -First 1 -ExpandProperty Id
+
+    # Deploying this environment incrementally so the template specs don't disappear.
+    # Otherwise, we have a Chicken/Egg scenario.
+    Write-Host ""; Write-Host "Creating storage resources in the management group to host the DSC configuration and application code."
+    $ManagementRGDeploymentParameters = @{
+        ResourceGroupName = $ManagementResourceGroupName
+        TemplateSpecId    = $ManagementRGSpecId
+        Mode              = 'Incremental'
+        Force             = $true
+    }
+
+    New-AzResourceGroupDeployment @ManagementRGDeploymentParameters
+
+    Write-Host ""; Write-Host 'Staging the application files to the storage account in the management resource group.'
+    $Storage = Get-AzStorageAccount -ResourceGroupName $ManagementResourceGroupName -Name $StorageAccountName
+    Set-AzStorageBlobContent -File $ApplicationArchivePath -Container $StorageContainerName -Context $Storage.Context | Out-Null
+
+    Write-Host ""; Write-Host 'Getting the xWebAdministration module to package as part of the published DSC configuration.'
+    if (-not (Get-Module -ListAvailable xWebAdministration)) {
+        Install-Module xWebAdministration -RequiredVersion 3.2.0 -Scope CurrentUser
+    }
+
+    Write-Host ""; Write-Host 'Packaging and publishing the DSC configuration and supporting modules.'
+    $Parameters = @{
+        ResourceGroupName  = $ManagementResourceGroupName
+        ConfigurationPath  = $ConfigurationPath
+        StorageAccountName = $StorageAccountName
+        ContainerName      = $StorageContainerName
+    }
+    Publish-AzVMDscConfiguration @Parameters | Out-Null
 }
-
-# Deploy templates to template specs
-New-AzTemplateSpec -Name MercuryHealthWeb -TemplateFile ./azuredeploy.json @TemplateSpecParams
-New-AzTemplateSpec -Name DscStorage -TemplateFile ./DscStorage.json @TemplateSpecParams
-
-# 
-$DSCStorageSpec = "/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/$ResourceGroupName/providers/Microsoft.Resources/templateSpecs/DscStorage/versions/$($TemplateSpecParams.Version)"
-$MercuryHealthSpec = "/subscriptions/$((Get-AzContext).Subscription.Id)/resourceGroups/$ResourceGroupName/providers/Microsoft.Resources/templateSpecs/MercuryHealthWeb/versions/$($TemplateSpecParams.Version)"
-
-New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroupName -TemplateSpecId $DSCStorageSpec -Mode Complete -Force
-
-# Stage the application files
-$Storage = Get-AzStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
-Set-AzStorageBlobContent -File ../MercuryHealth.zip -Container $StorageContainerName -Context $Storage.Context
-
-if (-not (Get-Module -ListAvailable xWebAdministration)) {
-    Install-Module xWebAdministration -RequiredVersion 3.2.0 -Scope CurrentUser
-}
-
-$Parameters = @{
-    ResourceGroupName = $ResourceGroupName 
-    ConfigurationPath = $ConfigurationPath
-    StorageAccountName = $StorageAccountName
-    ContainerName = $StorageContainerName
-}
-
-Publish-AzVMDscConfiguration @Parameters
-
-$ExecutionContext.InvokeCommand.ExpandString((get-content './azuredeploy.parameters.json' -raw)) | 
-    out-file current.parameters.json -Force
-$FullDeploymentParameters = @{
-    ResourceGroupName = $ResourceGroupName
-    TemplateSpecId = $MercuryHealthSpec 
-    TemplateParameterFile = 'current.parameters.json'
-    Mode = 'Complete'
-    Force = $true
-}
-New-AzResourceGroupDeployment @FullDeploymentParameters 
-
-
-<#
-# Helpers
-
-
-Set-AzVMDscExtension -ResourceGroupName $rg -VMName mercuryheathvm -ArchiveStorageAccountName optcdsc -ConfigurationName MercuryHealthWeb -ArchiveBlobName MercHealthConfig.ps1.zip -ArchiveContainerName configurations -Version "2.83"
-
-$template = @'
-WindowsFeature $Name {
-    Name = '$Name'
-    Ensure = 'Present'
-}
-'@
-$templatedep = @'
-WindowsFeature $Name {
-    Name = '$Name'
-    Ensure = 'Present'
-    DependsOn = $DependsOn
-}
-'@
-
-$f | 
-    sort depth | 
-    % { 
-        $Name = $_.Name
-        $currenttemplate = if ($_.DependsOn) { $templatedep; $DependsOn = "'[WindowsFeature]" + ($_.DependsOn -join "','[WindowsFeature]") + "'" } else { $template }
-        $ExecutionContext.InvokeCommand.ExpandString($currenttemplate)
-        }  | clip
+if (-not $ManagementRGOnly) {
     
-#>
+    Write-Host ""; Write-Host "Creating the application resource group."
+    New-AzResourceGroup -Name $ResourceGroupName -Location $Location -Force
+
+    $StorageAccount = Get-AzStorageAccount -ResourceGroupName $ManagementResourceGroupName -Name $StorageAccountName
+    $dscBlogStorageUri = $StorageAccount.PrimaryEndpoints.Blob + $StorageContainerName + '/'
+    Write-Host ""; Write-Host "Creating a current parameters file with $dscBlogStorageUri."
+    $ExecutionContext.InvokeCommand.ExpandString((get-content './ApplicationRG.parameters.json' -raw)) |
+        out-file current.parameters.json -Force
+
+    if (-not $ApplicationRGSpecId) {
+        Write-Host ""; Write-Host "Getting the current application resource group templatespec Id"
+        $TemplateSpecParams.Remove('Location') | Out-Null
+        $ApplicationRGSpecId = (Get-AzTemplateSpec @TemplateSpecParams -Name ApplicationRG).Versions |
+            Sort-Object -Property Name -Descending |
+            Select-Object -First 1 -ExpandProperty Id
+    }
+
+    Write-Host ""; Write-Host "Deploy the application environment."
+    $FullDeploymentParameters = @{
+        ResourceGroupName     = $ResourceGroupName
+        TemplateSpecId        = $ApplicationRGSpecId
+        TemplateParameterFile = 'current.parameters.json'
+        Mode                  = 'Complete'
+        Force                 = $true
+    }
+    New-AzResourceGroupDeployment @FullDeploymentParameters
+}
