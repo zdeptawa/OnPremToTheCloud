44bc8872ba64dd30d40b7455fc4561487e35eccb
Zachary Deptawa <zdeptawa@gmail.com>
2021-03-05 17:35:15 -0600

    adding force to install of modules

--- Infrastructure/taskBuildDsc.ps1
+++ Infrastructure/taskBuildDsc.ps1
@@ -2,11 +2,11 @@ $ConfigurationPath = './Infrastructure/MercHealthConfig.ps1'
 
 Write-Host ""; Write-Host 'Getting the xWebAdministration module to package as part of the published DSC configuration.'
 if (-not (Get-Module -ListAvailable xWebAdministration)) {
-    Install-Module xWebAdministration -RequiredVersion 3.2.0 -Scope CurrentUser
+    Install-Module xWebAdministration -RequiredVersion 3.2.0 -Scope CurrentUser -Force
 }
 Write-Host ""; Write-Host 'Getting the xPSDesiredStateConfiguration module to package as part of the published DSC configuration.'
 if (-not (Get-Module -ListAvailable xPSDesiredStateConfiguration)) {
-    Install-Module xPSDesiredStateConfiguration -RequiredVersion 9.1.0 -Scope CurrentUser
+    Install-Module xPSDesiredStateConfiguration -RequiredVersion 9.1.0 -Scope CurrentUser -Force
 }
 Write-Host ""; Write-Host 'Packaging and publishing the DSC configuration and supporting modules.'
 $Parameters = @{
